0f781f4e523aaa994bf8f327f1536b95008fbfdb
lucas-brisolla <99052177+lucas-brisolla@users.noreply.github.com>
2023-05-01 16:29:20 -0300

    click

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,3 @@ var task = document.getElementsByClassName("tasks")
 var add_task = document.getElementsByClassName("add-task-button")
 add_task.addEventListener("click", button)
 
-function button() {
-  document.body.innerHTML = "<h1>Tarefas</h1>"
-}
